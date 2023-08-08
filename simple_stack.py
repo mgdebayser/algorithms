3b631cfa03b61bec887553d3086603ff05d30fc8
Maira Gatti de Bayser <gattidebayser@gmail.com>
2023-08-08 14:54:05 -0300

    Added constraints and edge cases

--- simple_stack.py
+++ simple_stack.py
@@ -1,7 +1,7 @@
 # Encontre a posição mais profunda de um elemento em uma pilha de números
 #
 # Dados de entrada: 
-# A pilha que contém elementos E. 
+# A pilha de tamanho N que contém elementos E. 
 # Um inteiro x, cuja posição mais profunda é a ser encontrada.
 #
 # Dados de saída:
@@ -9,14 +9,27 @@
 #
 # Restrições
 # 1 <= N <= 100
-# 1 <= E, c <= 100.000.000
+# 1 <= E, x <= 100.000.000
 
 def simple_stack(stack, x):
     result = 0
     index = len(stack)
-    for i in range(0,len(stack)-1):
-        if stack.pop() == x:
+    if index < 1 or index > 100:
+        print("O tamanho da pilha tem que ser entre 1 e 100.")
+        return False
+    
+    if x < 1 or x > 100000000:
+            print("O elemento buscado precisa ser um inteiro entre 1 e 10ˆ8.")
+            return False
+    
+    for i in range(0,len(stack)):
+        e = stack.pop()
+        if e < 1 or e > 100000000:
+            print("Todos os elementos da pilha precisam ser inteiros entre 1 e 10ˆ8.")
+            return False
+        if e == x:
             result = i
+
     return result+1
 
 if __name__ == "__main__":
@@ -25,3 +38,13 @@ if __name__ == "__main__":
     print(stack)
     result = simple_stack(stack, 10)
     print("O índice mais profundo da pilha é: ", result)
+
+    print("A pilha é:")
+    stack = [-1,20,10]
+    print(stack)
+    result = simple_stack(stack, 10)
+
+    print("A pilha é:")
+    stack = [10,20,10]
+    print(stack)
+    result = simple_stack(stack, -1)
